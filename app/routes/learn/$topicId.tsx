7991d96518829a31de416abb4a679ac4260a1b3e
Reilly <reillyodie@gmail.com>
2022-06-03 10:03:09 -0400

    Data is fetched in Learn route based on topic name of that route

--- app/routes/learn/$topicId.tsx
+++ app/routes/learn/$topicId.tsx
@@ -3,84 +3,45 @@ import { json } from '@remix-run/node';
 import { useLoaderData } from '@remix-run/react';
 import { useEffect, useState } from 'react';
 import TopicCard, { baseArticle } from '~/components/topic-card';
+import { db } from '~/utils/db.server';
 
-export const loader: LoaderFunction = async ({ params }) => {
-  const topic = { name: params.topicId };
-  return json(topic);
+type bothData = {
+  // of type LoaderData
+  data: LoaderData;
+  name: string;
 };
 
-interface ArticleData extends baseArticle {
-  id: number;
-}
-const data: Array<ArticleData> = [
-  {
-    title: 'React Fundamentals: Props vs State',
-    author: 'Kent C. Dodds',
-    link: 'https://kentcdodds.com/blog/props-vs-state',
-    id: 0,
-    upvotes: 69,
-    twitterHandle: 'kentcdodds',
-  },
-  {
-    title: 'When to useMemo and useCallback',
-    author: 'Kent C. Dodds',
-    link: 'https://kentcdodds.com/blog/usememo-and-usecallback',
-    id: 1,
-    upvotes: 426,
-    twitterHandle: 'kentcdodds',
-  },
-  {
-    title: 'How to React ⚛️',
-    author: 'Kent C. Dodds',
-    link: 'https://kentcdodds.com/blog/how-to-react',
-    id: 2,
-    upvotes: 780,
-    twitterHandle: 'kentcdodds',
-  },
-  {
-    title: 'How to use React Context effectively',
-    author: 'Kent C. Dodds',
-    link: 'https://kentcdodds.com/blog/how-to-use-react-context-effectively',
-    id: 3,
-    upvotes: 820,
-    twitterHandle: 'kentcdodds',
-  },
-  {
-    title: 'A Complete Guide To useEffect',
-    author: 'Dan Abramov',
-    link: 'https://overreacted.io/a-complete-guide-to-useeffect/',
-    id: 4,
-    upvotes: 70382,
-    twitterHandle: 'dan_abramov',
-  },
-  {
-    title: 'React Fundamentals: Props vs State',
-    author: 'Kent C. Dodds',
-    link: 'https://kentcdodds.com/blog/props-vs-state',
-    id: 5,
-    upvotes: 69949,
-    twitterHandle: 'kentcdodds',
-  },
-  {
-    title: 'Heres an uneccessarily long title wihosanoinasd oinsdfoinsdf',
-    author: 'anonymous',
-    link: '',
-    id: 6,
-    upvotes: 69420,
-  },
-  {
-    title: 'Exactly 1000 upvotes',
-    author: 'anonymous',
-    link: '',
-    id: 7,
-    upvotes: 1000,
-  },
-];
+type LoaderData = {
+  articlePosts: Array<{
+    id: string;
+    authorOfPost: string;
+    authorTwitter: string | null;
+    title: string;
+    url: string;
+    points: number;
+    topic: string;
+    createdAt: Date;
+  }>;
+};
+export const loader: LoaderFunction = async ({ params }) => {
+  const name = params.topicId;
+
+  const data: LoaderData = {
+    articlePosts: await db.post.findMany({
+      where: {
+        topic: name,
+      },
+    }),
+  };
+  return json({ data, name });
+};
 
 export default function JokeRoute() {
   const [mute, setMute] = useState(false);
-  const topic = useLoaderData();
-  const name: string = topic.name;
+  const { data, name } = useLoaderData<bothData>();
+
+  const { articlePosts } = data;
+  console.log(articlePosts);
 
   useEffect(() => {
     const mute = localStorage.getItem('mute');
@@ -97,7 +58,7 @@ export default function JokeRoute() {
   };
 
   return (
-    <div className="flex justify-center items-center flex-col ">
+    <div className="flex justify-center items-center flex-col ml-20 ">
       <div className="flex justify-center items-center">
         <span
           className="font-bold text-6xl mb-3 capitalize"
@@ -108,9 +69,10 @@ export default function JokeRoute() {
         >
           {name}
         </span>
-        <span onClick={toggleAudio} className="ml-12 text-2xl cursor-pointer">
+        {/* TODO: MOVE THE AUDIO TOGGLE TO THE NAV */}
+        {/* <span onClick={toggleAudio} className="ml-12 text-2xl cursor-pointer">
           {mute ? '🔇' : '🔈'}
-        </span>
+        </span> */}
       </div>
       <div className="flex flex-col text-xl mt-8">
         <button className=" px-10 py-4 rounded-lg border-4 border-white hover:border-accent-pink hover:bg-accent-pink-bg active:border-accent-pink active:bg-accent-pink-bg ">
@@ -119,24 +81,28 @@ export default function JokeRoute() {
       </div>
 
       <div className="flex flex-col ">
-        {data
-          .sort((a, b) => (a.upvotes < b.upvotes ? 1 : -1))
-          .map((item, index) => {
-            const rank = index + 1;
+        {articlePosts.length ? (
+          articlePosts
+            .sort((a, b) => (a.points < b.points ? 1 : -1))
+            .map((item, index) => {
+              const rank = index + 1;
 
-            return (
-              <TopicCard
-                mute={mute}
-                author={item.author}
-                title={item.title}
-                link={item.link}
-                upvotes={item.upvotes}
-                key={item.id}
-                rank={rank}
-                twitterHandle={item.twitterHandle}
-              />
-            );
-          })}
+              return (
+                <TopicCard
+                  mute={mute}
+                  author={item.authorOfPost}
+                  title={item.title}
+                  link={item.url}
+                  upvotes={item.points}
+                  key={item.id}
+                  rank={rank}
+                  twitterHandle={item.authorTwitter ?? ''}
+                />
+              );
+            })
+        ) : (
+          <span>😔 No articles for this topic, yet!</span>
+        )}
       </div>
     </div>
   );
